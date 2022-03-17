56e47a92ae82d19fd20249833e285be668534586
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-03-17 23:01:29 +0100

    changed badly named permission type to permission category because type should only be read/write/...

--- Admin/Routes/Web/Api.php
+++ Admin/Routes/Web/Api.php
@@ -13,7 +13,7 @@
 declare(strict_types=1);
 
 use Modules\Dashboard\Controller\ApiController;
-use Modules\Dashboard\Models\PermissionState;
+use Modules\Dashboard\Models\PermissionCategory;
 use phpOMS\Account\PermissionType;
 use phpOMS\Router\RouteVerb;
 
@@ -25,7 +25,7 @@ return [
             'permission' => [
                 'module' => ApiController::NAME,
                 'type'   => PermissionType::CREATE,
-                'state'  => PermissionState::BOARD,
+                'state'  => PermissionCategory::BOARD,
             ],
         ],
     ],
@@ -36,7 +36,7 @@ return [
             'permission' => [
                 'module' => ApiController::NAME,
                 'type'   => PermissionType::CREATE,
-                'state'  => PermissionState::COMPONENT,
+                'state'  => PermissionCategory::COMPONENT,
             ],
         ],
     ],
--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -13,7 +13,7 @@
 declare(strict_types=1);
 
 use Modules\Dashboard\Controller\BackendController;
-use Modules\Dashboard\Models\PermissionState;
+use Modules\Dashboard\Models\PermissionCategory;
 use phpOMS\Account\PermissionType;
 use phpOMS\Router\RouteVerb;
 
@@ -25,7 +25,7 @@ return [
             'permission' => [
                 'module' => BackendController::NAME,
                 'type'   => PermissionType::READ,
-                'state'  => PermissionState::DASHBOARD,
+                'state'  => PermissionCategory::DASHBOARD,
             ],
         ],
     ],
